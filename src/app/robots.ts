1b58983ed6df20e5260793cbd1f2a49a786c9294
Kevin Ortega <kevin.ortega2011@gmail.com>
2025-10-04 03:21:59 -0400

    fix: update robots.txt sitemap URL to correct domain
    - Change sitemap URL from kevin-resume.vercel.app to kevinortega.me
    - This should resolve Google Search Console sitemap validation issues

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://kevin-resume.vercel.app/sitemap.xml',
+    sitemap: 'https://kevinortega.me/sitemap.xml',
   };
 }
